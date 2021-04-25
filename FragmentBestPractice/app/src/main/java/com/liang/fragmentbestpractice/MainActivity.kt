b682a8168a84377f4578f57ff55032cbe08b63a6
Liang <wangliang1124@163.com>
2021-04-25 23:15:06 +0800

    add BroadcastReceiver demo

--- FragmentBestPractice/app/src/main/java/com/liang/fragmentbestpractice/MainActivity.kt
+++ FragmentBestPractice/app/src/main/java/com/liang/fragmentbestpractice/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.liang.fragmentbestpractice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var timeChangeReceiver: TimeChangeReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+        timeChangeReceiver = TimeChangeReceiver()
+        registerReceiver(timeChangeReceiver, intentFilter)
+
+        val button: Button = findViewById(R.id.button1)
+        button.setOnClickListener {
+            val intent = Intent("com.example.broadcast.MY_BROADCAST")
+            intent.setPackage(packageName)
+            sendBroadcast(intent)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(timeChangeReceiver)
+    }
+
+    inner class TimeChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Toast.makeText(context, "Time has changed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
